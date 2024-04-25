cb512eaa78d951cf886371ccb556c0683c7a2beb
BuildTools <unconfigured@null.spigotmc.org>
2024-04-25 13:38:56 +0200

    door

--- Assets/scripts/PlayerInteract.cs
+++ Assets/scripts/PlayerInteract.cs
@@ -7,29 +7,47 @@ public class PlayerInteract : MonoBehaviour
     public GameObject interactibleObject;
     public float interactValue;
     public float interactRange = 0.7f;
+    public bool canInteract = true;
+    public float interactCooldown = 0.5f;
+    public float interactTimer;
 
     public LayerMask InteractableLayer;
 
+    public Sprite[] doorSprites;
+
+
     void Start()
     {
-        
+        interactTimer = interactCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
         interactValue = Input.GetAxisRaw("Interact");
+        if (!canInteract)
+        {
+            interactTimer -= Time.deltaTime;
+            if (interactTimer < 0)
+            {
+                canInteract = true;
+                interactTimer = interactCooldown;
+            }
+        }
+
     }
 
     void FixedUpdate()
     {
-        if (interactValue > 0)
+        if (interactValue > 0 && canInteract)
         {
-            Interact();
+            canInteract = false;
+            StartCoroutine(Interact());
+
         }
     }
 
-    void Interact()
+    IEnumerator Interact()
     {
         Collider2D[] interactables = Physics2D.OverlapCircleAll(transform.position, interactRange, InteractableLayer);
 
@@ -41,6 +59,7 @@ public class PlayerInteract : MonoBehaviour
             InteractSuccessful(interactable);
 
         }
+        yield return null;
     }
 
     // does nothing
@@ -53,13 +72,30 @@ public class PlayerInteract : MonoBehaviour
     // different interactables items
     void InteractSuccessful(Collider2D interactable)
     {
-        switch (interactable.GetComponent<Renderer>().name)
+        switch (interactable.GetComponent<SpriteRenderer>().sprite.name)
         {
-            case string greenDot_0:
+            case "greenDot_0":
                 interactable.GetComponent<Renderer>().enabled = false;
                 interactable.GetComponent<Collider2D>().enabled = false;
                 Debug.Log("You interacted with a green Dot: It disappeared");
                 break;
+
+            case "simpleDoorSheet_0":
+                interactable.GetComponent<SpriteRenderer>().sprite = doorSprites[1];
+                interactable.GetComponent<Collider2D>().isTrigger = true;
+
+                Debug.Log("You opened a door");
+
+                break;
+            case "simpleDoorSheet_1":
+                interactable.GetComponent<SpriteRenderer>().sprite = doorSprites[0];
+                interactable.GetComponent<Collider2D>().isTrigger = false;
+
+                Debug.Log("You closed a door");
+
+                break;
+
+
             default:
                 Debug.Log("ERROR: Interacted with something unknown");
                 break;
